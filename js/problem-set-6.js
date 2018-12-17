a251c334c578b1922c99c399d320ff3dbbcffd48
Kborysiak <kborysiak11@gmail.com>
2018-12-17 18:28:54 -0500

    final

--- js/problem-set-6.js
+++ js/problem-set-6.js
@@ -43,7 +43,7 @@ function drawRectangle() {
   let rectangle = document.getElementById("canvas2").getContext("2d");
   let rectHeight;
   let rectWidth;
-  let rectX;
+  let resmileyFace;
   let rectY;
   //Add limits to width height x and y at home.
   do{rectHeight = Number(prompt("Height:"))
@@ -54,20 +54,20 @@ console.log(rectHeight)
 }while(rectWidth >= canvas2.width && rectWidth < 1);
 console.log(rectWidth)
 
-  do{rectX = Number(prompt("X-Coordinate:"))
-}while(rectX <= canvas2.width && rectX < 5)
-console.log(rectX)
+  do{resmileyFace = Number(prompt("X-Coordinate:"))
+}while(resmileyFace <= canvas2.width && resmileyFace < 5)
+console.log(resmileyFace)
 
 do{rectY = Number(prompt("Y-Coordinate:"))
 }while(rectY <= canvas2.height && rectY < 5)
 
-if(rectHeight > canvas2.height && rectWidth > canvas2.width && rectX > canvas2.width && rectY > canvas2.height){
+if(rectHeight > canvas2.height && rectWidth > canvas2.width && resmileyFace > canvas2.width && rectY > canvas2.height){
   alert("These rectangle dimensions are improper! Please try again!");
 }
 else{
   console.log(rectY)
   rectangle.clearRect(0, 0, canvas2.width, canvas2.height);
-  rectangle.strokeRect(rectX, rectY, rectWidth, rectHeight);
+  rectangle.strokeRect(resmileyFace, rectY, rectWidth, rectHeight);
 }
 
 
@@ -159,6 +159,21 @@ console.log(side1);
 console.log(side2);
 console.log(side3);
 
+if((side1*side1 + side2*side2 == side3*side3) && side1>0 && side2>0 && side3>0 && canvas4.width-triangleX-side1>=0 && canvas4.height-triangleY-side2>=0){
+  drawEnabled = true;
+}
+else{
+  alert("Those parameters will not fit the canvas.")
+}
+if(drawEnabled == true){
+  triangle.beginPath();
+  triangle.moveTo(triangleX, triangleY);
+  triangle.lineTo(triangleX, triangleY+side1);
+  triangle.lineTo(triangleX+side2, triangleY+side1);
+  triangle.lineTo(triangleX, triangleY);
+  triangle.closePath();
+  triangle.stroke();
+}
 
 
 
@@ -186,8 +201,43 @@ console.log(side3);
 
 function drawSmileyFace() {
   let  smileyFace = document.getElementById("canvas5").getContext("2d");
-  smileyFace.beginPath();
+  smileyFace.clearRect(0, 0, canvas5.width, canvas5.height);
 
+  let faceRadius = Number(prompt("Please enter a radius for the face"));
+  let eyeRadius;
+  let mouthRadius;
+  let smileyX = 10;
+  let smileyY= 10;
+  drawEnabled=false;
+
+  if(faceRadius >= 1 && faceRadius <= canvas5.width && faceRadius <= canvas5.height && Number.isInteger(faceRadius)){
+    drawEnabled = true;
+  }
+  else{
+    alert("Invalid Radius")
+  }
+eyeRadius = faceRadius*.1
+mouthRadius = faceRadius*.7
+
+smileyFace.beginPath();
+smileyFace.arc(faceRadius + 10, faceRadius + 10, faceRadius, 0, Math.PI * 2);
+smileyFace.stroke();
+smileyFace.closePath();
+
+smileyFace.beginPath();
+smileyFace.arc(faceRadius + 10 - faceRadius/3, faceRadius + 10 - faceRadius/4, eyeRadius,0, Math.PI*2);
+smileyFace.stroke();
+smileyFace.closePath();
+
+smileyFace.beginPath();
+smileyFace.arc(faceRadius + 10 + faceRadius/3, faceRadius + 10 - faceRadius/4, eyeRadius, 0, Math.PI*2);
+smileyFace.stroke();
+smileyFace.closePath();
+
+smileyFace.beginPath();
+smileyFace.arc(faceRadius + 10, faceRadius + 10, mouthRadius, 0, Math.PI);
+smileyFace.stroke();
+smileyFace.closePath();
 }
 
 /*
@@ -209,9 +259,40 @@ function drawSmileyFace() {
  */
 
 function drawStar() {
+  let starDrawing = document.getElementById("canvas6").getContext("2d");
+   starDrawing.clearRect(0,0,1024,760);
 
+   let outerRadius;
+   let innerRadius;
+   do {
+     outerRadius = Number(prompt("Outer Radius:"));
+   } while (isNaN(outerRadius));
+   do {
+     innerRadius = Number(prompt("Inner Radius:"));
+   } while (isNaN(innerRadius));
+
+   let degrees = 0;
+
+   if (innerRadius > outerRadius){
+     alert("Your outer radius must be larger than your inner radius.");
+   } else if (outerRadius<2){
+     alert("Your outer radius is too small.");
+   } else if (innerRadius<1){
+     alert("Your inner radius is too small");
+   } else {
+     starDrawing.beginPath();
+
+     starDrawing.moveTo(125,125-outerRadius);
+     for (let i=0; i<=5; i++){
+       starDrawing.lineTo(125+Math.round((Math.cos(Math.PI*(degrees-90)/180)*outerRadius)), 125+Math.round((Math.sin(Math.PI*(degrees-90)/180)*outerRadius)));
+       degrees +=36;
+       starDrawing.lineTo(125+Math.round((Math.cos(Math.PI*(degrees-90)/180)*innerRadius)), 125+Math.round((Math.sin(Math.PI*(degrees-90)/180)*innerRadius)));
+       degrees +=36;
+     }
+     starDrawing.stroke();
+     starDrawing.closePath();
+   }
 }
-
 /*
  * Stop Sign. 7 points.
  *
@@ -228,7 +309,30 @@ function drawStar() {
  */
 
 function drawStopSign() {
+  let stopSign = document.getElementById("canvas7").getContext("2d");
+  stopSign.clearRect(0, 0, canvas7.height, canvas7.width)
+  let side = 80;
+  let c = Number((80/Math.sqrt(2)).toFixed(0));
+  let stopX = 70;
+  let stopY = 10;
 
+  stopSign.beginPath();
+  stopSign.moveTo(stopX,stopY);
+  stopSign.lineTo(stopX+side,stopY);
+  stopSign.lineTo(stopX+side+c,stopY+c);
+  stopSign.lineTo(stopX+side+c,stopY+side+c);
+  stopSign.lineTo(stopX+side,stopY+side+c+c);
+  stopSign.lineTo(stopX,stopY+side+c+c);
+  stopSign.lineTo(stopX-c,stopY+side+c);
+  stopSign.lineTo(stopX-c,stopY+c);
+  stopSign.lineTo(stopX,stopY);
+  stopSign.fillStyle="red";
+  stopSign.fill();
+  stopSign.closePath();
+
+  stopSign.fillStyle="white";
+  stopSign.font="65px sans-serif";
+  stopSign.fillText("STOP", 22, 133);
 }
 
 /*
@@ -250,7 +354,23 @@ function drawStopSign() {
  */
 
 function drawPyramid() {
-
+  let pyramid = document.getElementById('canvas8').getContext("2d")
+    pyramid.clearRect(0, 0, canvas8.width, canvas8.height);
+    let length=Number(prompt("Please enter a side length"));
+    let pyrX = 10;
+    let pyrY = canvas8.height-10;
+    let i = 0;
+    lineNumber = 1;
+    while(i < 5){
+      for(let j = 0+lineNumber;j <= 5; j++){
+        pyramid.strokeRect(pyrX, pyrY - length, length, length);
+        pyrX += length;
+      }
+    pyrX = 10 + (length / 2) * lineNumber;
+    pyrY -= length;
+      lineNumber++;
+      i++;
+    }
 }
 
 /*
@@ -282,6 +402,62 @@ function drawPyramid() {
  * unsupported color.
  */
 
-function drawHouse() {
+function drawHouse(){let houseDraw = document.getElementById("canvas9").getContext("2d");
+  houseDraw.clearRect(0,0,canvas9.width, canvas9.height);
+  let houseColor = prompt("Enter a house color.");
+  let doorColor = prompt("Enter a door color.");
+  if ((houseColor == "blue" || houseColor == "brown"|| houseColor == "green" || houseColor == "orange" || houseColor == "purple" || houseColor == "red" || houseColor == "yellow") && (doorColor == "blue" || doorColor == "brown"|| doorColor == "green" || doorColor == "orange" || doorColor == "purple" || doorColor == "red" || doorColor == "yellow")){
+//housebody
+    houseDraw.fillStyle="black";
+    houseDraw.fillRect(150,300,724,450);
+    houseDraw.fillStyle=houseColor;
+    houseDraw.fillRect(151,301,722,448);
+    houseDraw.fill();
+//roofo
+    houseDraw.beginPath();
+    houseDraw.moveTo(150,300);
+    houseDraw.lineTo(510,10);
+    houseDraw.lineTo(860,300);
+    houseDraw.lineTo(150,300);
+    houseDraw.fillStyle="black";
+    houseDraw.fill();
+    houseDraw.closePath();
+    houseDraw.beginPath();
+    houseDraw.moveTo(150,300);
+    houseDraw.lineTo(510,12);
+    houseDraw.lineTo(870,300);
+    houseDraw.lineTo(150,300);
+    houseDraw.fillStyle="gray";
+    houseDraw.fill();
+    houseDraw.closePath();
+//window
+    houseDraw.fillStyle="black";
+    houseDraw.fillRect(260,620,80,80);
+    houseDraw.fillRect(260,400,80,80);
+    houseDraw.fillRect(684,620,80,80);
+    houseDraw.fillRect(684,400,80,80);
+    houseDraw.fillStyle="lightblue";
+    houseDraw.fillRect(261,620,80,80);
+    houseDraw.fillRect(261,400,80,80);
+    houseDraw.fillRect(685,620,80,80);
+    houseDraw.fillRect(685,400,80,80);
+//door
+    houseDraw.fillStyle="black"
+    houseDraw.fillRect(462,590,100,160);
+    houseDraw.fillStyle=doorColor;
+    houseDraw.fillRect(462,590,100,160);
+//doorknob
+    houseDraw.beginPath();
+    houseDraw.arc(545,670,8,0,Math.PI*2,true);
+    houseDraw.fillStyle="black";
+    houseDraw.fill();
+    houseDraw.closePath();
+    houseDraw.beginPath();
+    houseDraw.arc(545,670,7,0,Math.PI*2,true);
+    houseDraw.fillStyle="yellow";
+    houseDraw.fill();
+    houseDraw.closePath();
 
-}
+  } else {
+    alert("One of your colors is not supported");
+  }}
